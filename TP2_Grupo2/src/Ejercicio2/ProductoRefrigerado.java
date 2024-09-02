fc8f114a0c3975abc47d771f5a18de680ee6af01
CaroCabrera <165964647+CaroCabrera@users.noreply.github.com>
2024-09-02 01:34:53 -0300

    PUNTO 2 agregue constructores, Setters y Getters, y el metodo ToString() de ProductoRefrigerado

--- TP2_Grupo2/src/Ejercicio2/ProductoRefrigerado.java
+++ TP2_Grupo2/src/Ejercicio2/ProductoRefrigerado.java
@@ -4,6 +4,29 @@ public class ProductoRefrigerado extends Producto {
 	
 	private int codigoDelOrganismo;
 	
+	//constructor sin parametros
+	public ProductoRefrigerado() {}
 	
+	//constructor con parametros
+    public ProductoRefrigerado(String fechaDeCaducidad, int numeroDeLote, int codigoDelOrganismo) {
+        super(fechaDeCaducidad, numeroDeLote);
+        this.codigoDelOrganismo = codigoDelOrganismo;
+    }
+    //GETTERS & SETTERS
+    public int getCodigoDelOrganismo() {
+        return codigoDelOrganismo;
+    }
 
+    public void setCodigoDelOrganismo(int codigoDelOrganismo) {
+        this.codigoDelOrganismo = codigoDelOrganismo;
+    }
+    
+    //METODO TOSTRING()
+    @Override
+    public String toString() {
+        return "Producto Refrigerado" + 
+        		"\n Fecha de caducidad:" + getFechaDeCaducidad() + 
+        		"\n Numero de lote: " + getNumeroDeLote() +
+                "\n Código del organismo: " + codigoDelOrganismo;
+    }
 }
