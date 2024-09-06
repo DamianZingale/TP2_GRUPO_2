e4f02066017a64bf7976e76565bda0b8b7b6df67
CaroCabrera <165964647+CaroCabrera@users.noreply.github.com>
2024-09-06 19:24:57 -0300

    realice 2 interfaces (InstalacionDeportiva y Edificio) y una clase Polideportivo

--- TP2_Grupo2/src/Ejercicio3/InstalacionDeportiva.java
+++ TP2_Grupo2/src/Ejercicio3/InstalacionDeportiva.java
@@ -1,25 +1,5 @@
 package Ejercicio3;
 
-public class InstalacionDeportiva {
-	
-	private int TipoInstalacion;
+public interface InstalacionDeportiva {
 
-	public InstalacionDeportiva(int tipoInstalacion) {
-		super();
-		TipoInstalacion = tipoInstalacion;
-	}
-
-	public int getTipoInstalacion() {
-		return TipoInstalacion;
-	}
-
-	public void setTipoInstalacion(int tipoInstalacion) {
-		TipoInstalacion = tipoInstalacion;
-	}
-
-	@Override
-	public String toString() {
-		return "TipoInstalacion:" + TipoInstalacion;
-	}
-	
 }
